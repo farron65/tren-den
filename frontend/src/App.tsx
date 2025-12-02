977551f97992e97aa50c1be618ef67ac6c862218
farron65 <bezhansn@gmail.com>
2025-12-01 21:28:02 -0500

    Add protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Workouts from "./components/Workouts";
 import WorkoutPage from "./components/WorkoutPage";
 import CreateWorkout from "./components/WorkoutCreate";
 
+
+type ProtectedRouteProps = {
+    token: string | null;
+    redirectPath?: string;
+    children: React.ReactNode;
+}
+
+const ProtectedRoute = ({ token, redirectPath="/", children }: ProtectedRouteProps) => {
+    if (!token) {
+        return <Navigate to={redirectPath} replace/>
+    }
+    return children
+}
+
 function App() {
+    const access_token = localStorage.getItem("access_token");
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={"/signup"} element={<Signup />}></Route>
                 <Route path={"/"} element={<Signup />}></Route>
                 <Route path={"/login"} element={<Login />}></Route>
-                <Route path={"/workouts"} element={<Workouts />}></Route>
-                <Route path={"/workouts/:id"} element={<WorkoutPage/>}></Route>
-                <Route path={"/create-workout/"} element={<CreateWorkout/>}></Route>
+
+                <Route
+                    path={"/workouts"}
+                    element={<ProtectedRoute token={access_token}>
+                        <Workouts/>
+                    </ProtectedRoute>}>
+                </Route>
+                <Route
+                    path={"/workouts/:id"} 
+                    element={<ProtectedRoute token={access_token}>
+                        <WorkoutPage />
+                    </ProtectedRoute>}>
+                </Route>
+                <Route 
+                    path={"/create-workout/"}
+                    element={<ProtectedRoute token={access_token}>
+                        <CreateWorkout />
+                    </ProtectedRoute>}>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
