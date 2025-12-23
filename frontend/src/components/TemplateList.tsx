6ca17ec127c4fa8ec8810da528a1cb81133816e0
farron65 <bezhansn@gmail.com>
2025-12-22 23:28:20 -0500

    Add workout template deletion through backend endpoint

--- frontend/src/components/TemplateList.tsx
+++ frontend/src/components/TemplateList.tsx
@@ -1,11 +1,41 @@
 import { useEffect, useState } from "react";
 
+interface Exercise {
+    id: number,
+    exercise_name: string,
+}
+
+interface Template {
+    id: number
+    workout_name: string,
+    exercises: Exercise[]
+}
+
 export default function ListTemplates() {
-    const [templates, setTemplates] = useState<any[]>([]);
+    const [templates, setTemplates] = useState<Template[]>([]);
     const access_token = localStorage.getItem("access_token");
     const url = "http://127.0.0.1:8000/templates"
 
-    useEffect(() => {
+    async function handleDelete(targetId: number) {
+        const headers = {"Authorization": `Bearer ${access_token}`}
+
+        try {
+            const response = await fetch(`${url}/${targetId}`, {method: "DELETE", headers: headers});
+            
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            else {
+                const updatedTemplate = templates.filter((template) => template.id != targetId);
+                setTemplates(updatedTemplate);
+            }
+
+        }
+        catch (error) {
+            alert(error);
+        }
+    }
+    useEffect(() => {   
 
         const fetchTemplates = async () => {
             if (!access_token) {
@@ -30,5 +60,24 @@ export default function ListTemplates() {
         fetchTemplates();
     }, []);
 
-    return <div>Template</div>
+    return (
+        <>
+        <h1>Templates</h1>
+            {templates.map((template) => (
+                <div key={template.id}>
+                    <h2>
+                        {template.workout_name}
+                    </h2>
+                    {template.exercises.map((exercise: any) => (
+                        <div key={exercise.id}>
+                            <p>
+                                {exercise.exercise_name}
+                            </p>
+                        </div>
+                    ))}
+                <button onClick={() => handleDelete(template.id)}>Delete</button>
+                </div>
+            ))}
+        </>
+    )
 }
