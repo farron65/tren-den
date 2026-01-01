9684333354b175e0530955123132de4dc4dae555
farron65 <bezhansn@gmail.com>
2025-12-31 22:45:55 -0500

    Fix: in GET /recente/exercises change name comparison to lowercase to identify unique exercises

--- backend/main.py
+++ backend/main.py
@@ -198,9 +198,7 @@ async def get_exercise_data(exercise_name: str, current_user: Annotated[User, De
 
 @app.get("/recent/exercises/") 
 async def get_recent_exercises(current_user: Annotated[User, Depends(get_current_active_user)], session: SessionDep):
-    user_exercises = session.exec(select(Exercise).join(Workout).where(Workout.user == current_user).order_by(desc(Workout.date)).limit(20)).all() # type: ignore
-    print(len(user_exercises))
-    
+    user_exercises = session.exec(select(Exercise).join(Workout).where(Workout.user == current_user).order_by(desc(Workout.date)).limit(25)).all() # type: ignore
     unique_ex_names = set()
     
     user_exercises_data = []
@@ -209,8 +207,8 @@ async def get_recent_exercises(current_user: Annotated[User, Depends(get_current
         raise HTTPException(404, "Not Found")
 
     for unique_ex in user_exercises:
-        if unique_ex.exercise_name not in unique_ex_names:
-            unique_ex_names.add(unique_ex.exercise_name)
+        if unique_ex.exercise_name.lower() not in unique_ex_names:
+            unique_ex_names.add(unique_ex.exercise_name.lower())
             temp = {"id": unique_ex.id, "exercise_name": unique_ex.exercise_name, "sets": []}
             for ex_set in unique_ex.sets:
                 temp["sets"].append({"id": ex_set.id, "weight": ex_set.weight, "reps": ex_set.reps})
