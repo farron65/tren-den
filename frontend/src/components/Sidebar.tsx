176213545aa8cd038f6bd8c84f5ef0ee69cd03ad
farron65 <bezhansn@gmail.com>
2026-01-07 17:23:50 -0500

    Change css styles, and a collapse button

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,18 +1,31 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 import "./sidebar.css";
+import hamburgerIcon from "../assets/hamburger.png";
 
 export default function Sidebar() {
-    return (
-        <aside className="sidebar">
-            <h2 className="logo">TRENDEN</h2>
+  const [collapsed, setCollapsed] = useState(false);
 
-            <nav className="nav">
-                <NavLink to="/workouts">Home</NavLink>
-                <NavLink to="/create-workout">Start Workout</NavLink>
-                <NavLink to="/templates">Templates</NavLink>
-                <NavLink to="/analytics">Analytics</NavLink>
-            </nav>
-        </aside>
-    );
+  return (
+    <aside className={`sidebar ${collapsed ? "collapsed" : ""}`}>
+      <div className="sidebar-top">
+        <h2 className="logo">TRENDEN</h2>
+
+        <button
+          className="collapse-btn"
+          onClick={() => setCollapsed((v) => !v)}
+        >
+          <img src={hamburgerIcon} alt="navbar button" />
+        </button>
+      </div>
+
+      <nav className="nav">
+        <NavLink to="/workouts">Home</NavLink>
+        <NavLink to="/create-workout">Start Workout</NavLink>
+        <NavLink to="/templates">Templates</NavLink>
+        <NavLink to="/analytics">Analytics</NavLink>
+      </nav>
+    </aside>
+  );
 }
