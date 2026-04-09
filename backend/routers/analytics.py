f050fd1191e27e048e0f76e24f62b828fdf0325f
farron65 <bezhansn@gmail.com>
2026-04-09 12:00:56 -0400

    feat: add search exercise endpoint

--- backend/routers/analytics.py
+++ backend/routers/analytics.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
-from sqlmodel import select
+from sqlmodel import select, distinct
 from sqlalchemy import func, desc, asc
 from sqlalchemy.orm import selectinload
 
@@ -12,6 +12,40 @@ from auth_utils import get_current_active_user
 from queries import get_exercise_sets, get_exercise_history, compare_previous_values
 
 router = APIRouter()
+
+@router.get("/")
+async def search_exercises(exercise_name: str, current_user: Annotated[User, Depends(get_current_active_user)], session: SessionDep):
+    if not exercise_name:
+        raise HTTPException(400, "Invalid Exercise Name")
+    user_exercises = session.exec(select(Exercise.exercise_name, func.max(Workout.date)) # You can't order by the things you haven't 'select' -ed
+                        .where(func.lower(Exercise.exercise_name).contains(exercise_name))
+                        .join(Workout).where(Workout.user == current_user)
+                        .order_by(desc(func.max(Workout.date))).group_by(Exercise.exercise_name)).all() # type: ignore
+
+    return dict(user_exercises)
+
+@router.get("/recent") 
+async def get_recent_exercises(current_user: Annotated[User, Depends(get_current_active_user)], session: SessionDep):
+    user_exercises = session.exec(select(Exercise).join(Workout)
+        .where(Workout.user == current_user)
+        .options(selectinload(Exercise.sets)) # type: ignore
+        .order_by(desc(Workout.date)).limit(25)).all() # type: ignore
+    
+    unique_ex_names = set()
+    user_exercises_data = []
+    
+    if not user_exercises:
+        return []
+
+    for unique_ex in user_exercises:
+        if unique_ex.exercise_name.lower() not in unique_ex_names:
+            unique_ex_names.add(unique_ex.exercise_name.lower())
+            temp = {"id": unique_ex.id, "exercise_name": unique_ex.exercise_name, "rest_time": unique_ex.rest_time, "sets": []}
+            for ex_set in unique_ex.sets:
+                temp["sets"].append({"id": ex_set.id, "weight": ex_set.weight, "reps": ex_set.reps})
+            user_exercises_data.append(temp)
+        
+    return user_exercises_data
 
 @router.get("/analytics/{exercise_name}")
 async def get_exercise_analytics(exercise_name: str, current_user: Annotated[User, Depends(get_current_active_user)], session: SessionDep):
@@ -64,29 +98,6 @@ async def get_exercise_analytics(exercise_name: str, current_user: Annotated[Use
         "data": user_exercise_data,
         "summary": summary
     }
-
-@router.get("/recent") 
-async def get_recent_exercises(current_user: Annotated[User, Depends(get_current_active_user)], session: SessionDep):
-    user_exercises = session.exec(select(Exercise).join(Workout)
-        .where(Workout.user == current_user)
-        .options(selectinload(Exercise.sets)) # type: ignore
-        .order_by(desc(Workout.date)).limit(25)).all() # type: ignore
-    
-    unique_ex_names = set()
-    user_exercises_data = []
-    
-    if not user_exercises:
-        return []
-
-    for unique_ex in user_exercises:
-        if unique_ex.exercise_name.lower() not in unique_ex_names:
-            unique_ex_names.add(unique_ex.exercise_name.lower())
-            temp = {"id": unique_ex.id, "exercise_name": unique_ex.exercise_name, "rest_time": unique_ex.rest_time, "sets": []}
-            for ex_set in unique_ex.sets:
-                temp["sets"].append({"id": ex_set.id, "weight": ex_set.weight, "reps": ex_set.reps})
-            user_exercises_data.append(temp)
-        
-    return user_exercises_data
 
 @router.get("/{exercise_name}")
 async def get_exercise_data(exercise_name: str, current_user: Annotated[User, Depends(get_current_active_user)], session: SessionDep):
